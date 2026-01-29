7ee0739256f93f55c81c4fef2f587dd9cfc61ae0
thu tam <ngocvotam27@gmail.com>
2026-01-29 20:55:36 +0700

    update recaptcha

--- frontend/src/components/ServiceRequest.jsx
+++ frontend/src/components/ServiceRequest.jsx
@@ -2,10 +2,9 @@ import { FiUser } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
 import { FiPhone } from "react-icons/fi";
 import { LuBookOpen } from "react-icons/lu";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import ReCAPTCHA from "react-google-recaptcha";
-import React from 'react';
 
 const GOOGLE_RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
   
@@ -18,23 +17,8 @@ const ServiceRequest = () => {
   const [ phoneError, setPhoneError ] = useState(false);
   const [ service, setService ] = useState("");
   const [ note, setNote ] = useState("");
-  const recaptchaRef = useRef(null);
 
-  const resetForm = () => {
-    setName("");
-    setEmail("");
-    setEmailError(false);
-    setPhone("");
-    setPhoneError(false);
-    setService("");
-    setNote("");
-    setVerify(false);
-    if (recaptchaRef.current) {
-      recaptchaRef.current.reset();
-    }
-  };
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     // Validate form fields here if needed
     event.preventDefault();
     if (!name || !email || !phone || !service) {
@@ -56,47 +40,34 @@ const ServiceRequest = () => {
       return;
     }
     // Here you can handle the form submission, e.g., send data to a server
-    const apiUrl = "https://stylice.onrender.com/api/v1/service"
     const formData = {
-      name,
-      email,
-      phone,
-      service,
-      note
+      name: name,
+      email: email,
+      phone: phone,
+      service: service,
+      note: note
     };
-    fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      console.log("Response status:", response);
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Form Data Submitted:", formData);
-      console.log("Success:", data);
-      // Reset form
-      resetForm();
-      alert("Submit thành công.");
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      alert("Đã xảy ra lỗi khi gửi dữ liệu. Vui lòng thử lại sau.");
-    });
+    console.log("Form Data Submitted:", formData);
+    alert("Submit thành công.");
+    // Reset form fields - reload page
+    
   }
 
+  const handleRecapcha = (value) => {
+    if (value) {
+      setVerify(true);
+      console.log("reCAPTCHA verified:", value);
+    } else {
+      setVerify(false);
+      console.log("reCAPTCHA not verified");
+    }
+  }
   return (
-    <div className="mt-20 mb-20 rounded-lg shadow-2xl flex h-fit text-black bg-white w-full sm:w-1/2 flex-col items-center justify-center">
+    <div className="mt-20 mb-20 rounded-lg shadow-2xl flex h-fit text-black bg-white w-3/4 flex-col items-center justify-center">
       <form className="pt-24 pb-24 flex flex-col items-center justify-center p-4 w-full">
         <div className="flex items-center mb-2 justify-center w-full">
           <div className="relative items-start flex flex-col w-5/6">
-            <FiUser className="absolute ml-3 top-6 -translate-y-1/2 text-black-800"/>
+            <FiUser className="absolute left-1/40 top-6 transform -translate-y-1/2 text-black-800"/>
             <input
               type="text"
               value={name}
@@ -108,7 +79,7 @@ const ServiceRequest = () => {
         </div>
         <div className="flex items-center mb-2 justify-center w-full">
           <div className="relative items-start flex flex-col w-5/6">
-            <MdOutlineEmail className="absolute ml-3 top-6 -translate-y-1/2 text-black-800"/>
+            <MdOutlineEmail className="absolute left-1/40 top-6 transform -translate-y-1/2 text-black-800"/>
             <input
               type="text"
               value={email}
@@ -126,7 +97,7 @@ const ServiceRequest = () => {
         </div>
         <div className="flex items-center mb-2 justify-center w-full">
           <div className="relative items-start flex flex-col w-5/6">
-            <FiPhone className="absolute ml-3 top-6 -translate-y-1/2 text-black-800"/>
+            <FiPhone className="absolute left-1/40 top-6 transform -translate-y-1/2 text-black-800"/>
             <input
               type="text"
               value={phone}
@@ -144,7 +115,7 @@ const ServiceRequest = () => {
         </div>
         <div className="flex items-center mb-2 justify-center w-full">
           <div className="relative items-start flex flex-col w-5/6">
-            <LuBookOpen className="absolute ml-3 top-6 -translate-y-1/2 text-black-800"/>
+            <LuBookOpen className="absolute left-1/40 top-6 transform -translate-y-1/2 text-black-800"/>
             <input
               type="text"
               value={service}
@@ -156,7 +127,7 @@ const ServiceRequest = () => {
         </div>
         <div className="flex items-center mb-2 justify-center w-full">
           <div className="relative items-start flex flex-col w-5/6">
-            <FaRegEdit className="absolute ml-3 top-6 -translate-y-1/2 text-black-800"/>
+            <FaRegEdit className="absolute left-1/40 top-6 transform -translate-y-1/2 text-black-800"/>
             <textarea
               placeholder="Any Note For Us"
               value={note}
@@ -166,16 +137,13 @@ const ServiceRequest = () => {
           </div>
         </div>
         <ReCAPTCHA
-          ref={recaptchaRef}
           className="mb-4 w-5/6 flex items-center justify-center"
           sitekey={GOOGLE_RECAPTCHA_SITE_KEY}
-          onChange={()=> setVerify(!verify)}
-          onExpired={() => setVerify(false)}
-          size="normal"
+          onChange={handleRecapcha}
         />
         <button
           onClick={handleSubmit}
-          className="cursor-pointer bg-[#422A3C] text-white px-4 py-4 rounded w-5/6"
+          className="text-white px-4 py-4 rounded w-5/6"
         >
           SUBMIT
         </button>
